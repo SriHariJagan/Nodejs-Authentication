5a8e1c6484e3fb61fc2fbf7a7c446804342bafd2
Sri Hari Jagan <100404902+SriHariJagan@users.noreply.github.com>
2024-03-20 21:48:57 +0530

    mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const url = "mongodb+srv://sriharijagan333:o8J4s6T2i23my8q1@cluster0.2xscnr7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+mongoose.connect(url);
 const db = mongoose.connection;
 
 db.on('error',(err) => {console.log(err)});
